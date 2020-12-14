be610c3158480357015e00bfa09679762970089a
John Manack <jmnck28@gmail.com>
2020-12-14 16:15:49 -0500

    Update pets file

--- class_conversion_pets.py
+++ class_conversion_pets.py
@@ -24,8 +24,17 @@ class Pet:
         """ % (self.name, self.fullness, self.happiness)
         
 class CuddlyPet(Pet):
+    def __init__(self, name, fullness=50, hunger=5, cuddle_level=1):
+        super().__init__(name, fullness, 100, hunger, 1)
+        self.cuddle_level = cuddle_level
+        
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness/2
+        
     def cuddle(self, other_pet):
-        other_pet.get_love()
+        for i in range(self.cuddle_level):
+            other_pet.get_love()
 
 benji = Pet("Benji", 50, 20, 20, 1)
 lassie = Pet("Lassie", 50, 20, 20, 1)
